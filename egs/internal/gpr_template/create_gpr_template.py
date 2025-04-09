ad0ddb65f7e92e61a9ccffc078d125a47c505d8a
Rajendra <rajendra@aveshasystems.com>
2025-04-09 12:08:56 +0530

    fix(): SDK changes for GPR Template

--- egs/internal/gpr_template/create_gpr_template.py
+++ egs/internal/gpr_template/create_gpr_template.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from egs.util.string_util import serialize
 
 class CreateGprTemplateRequest:
@@ -12,10 +13,10 @@ class CreateGprTemplateRequest:
         instance_type: str,
         exit_duration: str,
         priority: int,
-        idle_timeout_duration: str,
         enforce_idle_timeout: bool,
         enable_eviction: bool,
         requeue_on_failure: bool,
+        idle_timeout_duration: Optional[str] = None,
     ):
         self.name = name
         self.clusterName = cluster_name
@@ -26,10 +27,12 @@ class CreateGprTemplateRequest:
         self.instanceType = instance_type
         self.exitDuration = exit_duration
         self.priority = priority
-        self.idleTimeOutDuration = idle_timeout_duration
         self.enforceIdleTimeOut = enforce_idle_timeout
         self.enableEviction = enable_eviction
         self.requeueOnFailure = requeue_on_failure
+
+        if enforce_idle_timeout:
+            self.idleTimeOutDuration = idle_timeout_duration
 
     def __str__(self):
         return serialize(self)
