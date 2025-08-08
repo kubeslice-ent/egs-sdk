ebaea1e92ec28dccdf3207373ffdb99c5bd8f7dd
TezzBhandari <tejendra.bhandari@aveshasystems.com>
2025-08-08 13:00:51 +0545

    feat(): changes create gpr payload

--- egs/internal/gpr/create_gpr_data.py
+++ egs/internal/gpr/create_gpr_data.py
@@ -1,26 +1,35 @@
+from typing import List, Optional
+
 from egs.util.string_util import serialize
+
 
 class CreateGprRequest(object):
     def __init__(
-            self,
-            request_name: str,
-            workspace_name: str,
-            cluster_name: str,
-            node_count: int,
-            gpu_per_node_count: int,
-            memory_per_gpu: int,
-            instance_type: str,
-            gpu_shape: str,
-            exit_duration: str,
-            priority: int,
-            idle_timeout_duration: str,
-            enforce_idle_timeout: bool,
-            enable_eviction: bool,
-            requeue_on_failure: bool,
+        self,
+        request_name: str,
+        workspace_name: str,
+        node_count: int,
+        gpu_per_node_count: int,
+        memory_per_gpu: int,
+        exit_duration: str,
+        priority: int,
+        idle_timeout_duration: str,
+        enforce_idle_timeout: bool,
+        cluster_name: str = "",
+        instance_type: str = "",
+        gpu_shape: str = "",
+        enable_eviction: Optional[bool] = None,
+        requeue_on_failure: Optional[bool] = None,
+        prefered_cluster: Optional[List[str]] = None,
+        enable_auto_cluster_selection: bool = False,
+        enable_auto_gpu_selection: bool = False,
     ):
         self.gprName = request_name
         self.sliceName = workspace_name
         self.clusterName = cluster_name
+        self.preferedCluster = prefered_cluster
+        self.enableAutoClusterSelection = enable_auto_cluster_selection
+        self.enableAutoGPUSelection = enable_auto_gpu_selection
         self.numberOfGPUs = gpu_per_node_count
         self.instanceType = instance_type
         self.exitDuration = exit_duration
@@ -33,9 +42,9 @@ class CreateGprRequest(object):
         self.enableEviction = enable_eviction
         self.requeueOnFailure = requeue_on_failure
 
-
     def __str__(self):
         return serialize(self)
+
 
 class CreateGprResponse(object):
     def __init__(self, gprId: str, *args, **kwargs):
